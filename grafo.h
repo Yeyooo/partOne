f2202fe59d7f331c834df117edc135d79c15d340
Yeyooo <yeyo.cs@gmail.com>
2015-09-07 22:58:07 -0300

    Primer merge
    Definicion parcial de estructura a utilizar +  matriz que indica enlaces
    para mi grafo de tipo DIRIGIDO

--- grafo.h
+++ grafo.h
@@ -1,1 +1,36 @@
+#ifndef STDIO_H
+#include <stdio.h>
+#define STDIO_H
+#endif
 
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
+
+typedef struct t_nodo{
+    char color;// W white, B black, G grey;
+    struct t_nodo *padre;
+    // creo que como los enlaces pueden ser mas de uno se podria hacer con una estructura dinamica aparte, si es para un caso determinado yo determino el numero.
+    int distancia;//tiempo
+    char etiqueta;//simbolo o contenido?
+}Nodo;
+
+const int matrizEnlaces[8][8]={{0,1,0,1,0,0,0,0}, // las posiciones 0, 1, 2, 3... 7 en el grafo son a, b, c, d... h respectivamente
+                        {0,0,1,0,0,1,0,0},
+                        {0,0,0,1,0,1,1,1},
+                        {0,0,0,0,0,0,0,1},
+                        {0,0,0,0,0,0,0,0},
+                        {0,0,0,0,1,0,1,0},
+                        {0,0,0,0,0,0,0,1},
+                        {0,0,0,0,0,0,0,0}};
+
+
+void mostrarMatriz(){
+    int i,j;
+    for(i=0;i<8;i++){
+        for(j=0;j<8;j++)
+            printf(" %d ", matrizEnlaces[i][j]);
+        printf("\n\n");
+    }
+}
